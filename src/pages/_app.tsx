cd0622a0a1b5b3ee015dc32fbd9ab07127d97a61
Kacper Szot <kacperszot1@gmail.com>
2023-11-24 21:56:55 +0100

    make site fill full viewport

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,15 +3,19 @@ import React from 'react';
 import {ConfigProvider, Layout} from 'antd';
 import theme from "@/theme/themeConfig";
 import type {AppProps} from 'next/app';
+import {Header} from "antd/es/layout/layout";
 
 const App = ({Component, pageProps}: AppProps) => (
 
     <>
-    <ConfigProvider theme={theme}>
-        <Layout>
-            <Component {...pageProps} />
-        </Layout>
-    </ConfigProvider>
+        <ConfigProvider theme={theme}>
+            <Layout style={{
+                width: "100vw",
+                minHeight: "100vh",
+            }}>
+                <Component {...pageProps} />
+            </Layout>
+        </ConfigProvider>
     </>
 );
 
